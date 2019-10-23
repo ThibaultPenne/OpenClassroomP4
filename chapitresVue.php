aded8df577584b842f8bb758b6264a0111a0c4de
Thibault Penne <thibault.penne@gmail.com>
2019-10-23 02:12:25 +0200

    Ajout sur la page chapitresVue.php : img + vrais commentaires + vrai texte

--- chapitresVue.php
+++ chapitresVue.php
@@ -40,7 +40,7 @@
                     <ul class="navbar-nav mr-auto">
 
                         <li class="nav-item">
-                            <a class="nav-link" href="indexVue.php">Home</a>
+                            <a class="nav-link" href="indexVue.php">Accueil</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Bio</a>
@@ -120,15 +120,42 @@
                         <h4>Chapitre 1</h4>
                         <h6>Titre du chapitre 1</h6>
                         <hr>
-                        <p>
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York. <br>
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York.
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York. <br>
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York.
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York. <br>
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York. <br>
-                            Jean Forteroche est le second fils d'un couple d'avocats, il grandit à Santurce, un quartier de San Juan de Porto Rico. Sa mère meurt d'une hépatite alors qu'il est âgé de 9 ans, et sa famille déménage trois ans plus tard à Mercersburg dans la campagne de Pennsylvanie. À l'âge de 13 ans, Jean est inscrit dans une pension préparant aux meilleures écoles. Son père veut faire de lui un avocat. Il suit les conseils de son père et part étudier à l'université de Californie à San Diego, mais il abandonne rapidement pour entrer à la célèbre Square Acting School de New York.
-                        </p>
+                        <div class="chapContent">
+                            <p>
+                                Deux ans plus tard… <br>
+                                La journée avait été catastrophique, même avant que son passé ne décide de se réveiller et de lui cracher à la figure.
+                                En proie à un conflit d’émotions contradictoires, l’agent infiltré Reid Graham vit Lara Jordan faire son entrée au bras d’un cadre d’entreprise à l’allure stricte, dont le costume devait coûter deux fois son salaire gouvernemental mensuel.
+                                De tous les restaurants du pays, il avait fallu qu’elle vienne dans celui-ci, songea-t‑il. Et ce soir, par-dessus le marché !<br>
+                                Le désir et la colère le frappèrent à l’estomac à force égale. Le désir, parce que le souvenir de leur unique nuit deux ans auparavant constituait toujours son fantasme numéro un. Et la colère, parce qu’un simple mot d’elle pouvait griller sa couverture et mettre en péril une opération qui lui avait coûté des années de sang et de sueur. Une parole malvenue pouvait aisément les faire tuer tous les deux. Mais pas seulement. Balayant du regard le restaurant bondé, il fronça les sourcils devant tous ces gens qui, en un clin d’œil, pouvaient passer du statut d’innocents citoyens à celui de victimes de fusillade.<br>
+                                Nom de Dieu.<br>
+                                — Alors c’est vrai que vous n’êtes pas flic, dit Jen, la jeune blonde à la beauté froide assise en face de lui, tout en jouant avec le contenu de son assiette.<br>
+                                — Bien sûr que non, trésor.<br>
+                                Il ne mentait pas. Enfin, d’un point de vue technique.<br>
+                                — Je suis juste l’ami d’un ami, ajouta-t‑il avec un sourire décontracté. Je suis déjà passé par là où tu es. Après c’est plus facile, crois-moi.<br>
+                                Une musique de Noël passait en sourdine, tandis qu’une odeur de sapin flottait dans la salle. Le long des murs étaient exposées deux bonnes douzaines de compositions ornementales, lauréates d’un concours organisé dans les écoles du quartier à l’occasion des fêtes.<br>
+                            </p>
+                            <img src="images/chap01Img.png">
+                            <p>
+                                Tout en faisant semblant d’admirer la décoration, Reid jeta un nouveau regard en direction de Lara. Le visage rayonnant sous les lumières colorées, elle riait avec son compagnon. Ses cheveux étaient maintenant coupés à hauteur de menton, et leurs boucles impossibles dansaient contre ses joues, rehaussant la blancheur crémeuse de son cou dénudé.<br>
+                                Au fond de lui, il sentit quelque chose se nouer.<br>
+                                — Je veux me tirer, répliqua Jen en posant sa fourchette. Je veux disparaître. Je ne donnerai le CD que lorsque j’en aurai la garantie. Et je veux de l’argent.<br>
+                                — Je m’occupe de ça.<br>
+                                Ce soir, il était Dave Marshall, un personnage trouble qui opérait dans la zone grise entre les deux univers du bien et du mal, avec des liens dans chacun d’entre eux.<br>
+                                — Tu as quelque chose pour me prouver que tu es sérieuse ? demanda-t‑il.<br>
+                                Elle jeta un coup d’œil autour d’elle, puis sortit un téléphone noir de son sac et le glissa vers lui sur la table.<br>
+                                — C’est le portable de secours de Kenny. J’ai dit que le mien était cassé et le lui ai emprunté pour la journée.<br>
+                                Il se saisit de l’appareil et le fourra dans sa poche.<br>
+                                — Je te le rends demain matin. A part ça, tout va bien ?<br>
+                                Elle baissa les yeux et posa une main sur son ventre encore plat.<br>
+                                — Il n’est pas au courant, et je n’ai pas l’intention de le lui dire. Il s’est levé cette petite grue. Ce connard pense qu’il peut nous garder toutes les deux.<br>
+                                Elle renifla, la mine dégoûtée. Puis soupira.<br>
+                                — Ma sœur sait, ajouta-t‑elle en reposant sa main sur la table.<br>
+                                L’espaced’un instant, son chemisier bâilla et Reid aperçut le petit pistolet qu’elle portait sur elle. S’il en était besoin, cela lui rappela que Jen était une femme enceinte terrorisée, qui cherchait à quitter un petit ami terroriste infidèle. Même si elle jouait avec conviction le rôle de la jeune fille en détresse, elle n’était pas aussi pure que l’agneau qui vient de naître, loin s’en fallait.<br>
+                                — C’est à cause de lui que je me suis retrouvée dans ce sac de nœuds, reprit‑elle, une note de fragilité dans la voix. Tu me sors de là demain à la même heure, n’est‑ce pas ? Avant qu’ils ne remarquent la disparition du CD. Le Dr Julie dit que tu peux faire n’importe quoi.<br>
+                                Elle lui adressa un sourire qui promettait des moments voluptueux en guise de paiement.<br>
+                                Le Dr Julie Lantos — fournisseuse de soins d’urgence pour criminels blessés qui préféraient éviter les hôpitaux, et par ailleurs indicatrice — avait parlé de lui à Jen. Julie Lantos était aussi une toxicomane qui se fournissait auprès de ses patients, arrangement sur lequel les agents du FBI auxquels elle transmettait ses informations fermaient les yeux.
+                            </p>
+                        </div>    
                     </div>
                 </div>
             </div>
@@ -154,43 +181,35 @@
                 <div class="commentaires">
                     <div class="row justify-content-between">
                         <div class="col-5">
-                            <h4>de Daravann Penne</h4>
+                            <h4>de Inozuka</h4>
                         </div>
                         <div class="col-4">
-                            <h5>le 14/10/2019 à 16h30</h5>
+                            <h5>le 06/09/2019 à 19h30</h5>
                         </div>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Super merci !</p>
                 </div>
                 <div class="commentaires">
                     <div class="row justify-content-between">
                         <div class="col-5">
-                            <h4>de Mushashi Miyamoto</h4>
+                            <h4>de Itachi</h4>
                         </div>
                         <div class="col-4">
-                            <h5>le 14/10/2019 à 18h30</h5>
+                            <h5>le 07/09/2019 à 12h30</h5>
                         </div>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Hehehe ce roman s'annone incroyable ! Super idée !</p>
                 </div>
                 <div class="commentaires">
                     <div class="row justify-content-between">
                         <div class="col-5">
-                            <h4>de Blanche Neige</h4>
+                            <h4>de Senku</h4>
                         </div>
                         <div class="col-4">
-                            <h5>le 15/10/2019 à 11h30</h5>
+                            <h5>le 11/09/2019 à 21h21</h5>
                         </div>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat.</p>
+                    <p>Je pense que l'image n'aide pas forcément à l'immersion mais bon. Super chapitre, merci !</p>
                 </div>
             </div>
 
